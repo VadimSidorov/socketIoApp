501bef7b61c1174d45f3d768026698d68de15084
Vadim Sidorov <vadimsidorov@Vadims-MacBook-Pro.local>
2019-01-03 22:54:43 -0500

    add geolocation

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
@@ -12,3 +12,16 @@ describe('generateMessage', () => {
     expect(message).toInclude({from, text});
   });
 });
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    var from = 'Jen';
+    var latitude = 1;
+    var longitude = 1;
+    var message = generateLocationMessage(from, latitude,longitude)
+
+    expect(message.createdAt).toBeA('number');
+    expect(message.url).toBe('https://www.google.com/maps?q=1,1');
+  });
+});
+
